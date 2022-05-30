8b2aaf0590d9aa0c996a4a28c4dfffeb78f9b0a6
Ardiansyah <ardysmescocommunity@gmail.com>
2022-05-31 06:40:07 +0700

    build Adapter

--- app/src/main/java/com/example/jsongetsinggeluser/Adapter/AdapterData.java
+++ app/src/main/java/com/example/jsongetsinggeluser/Adapter/AdapterData.java
@@ -1,14 +1,18 @@
 package com.example.jsongetsinggeluser.Adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.jsongetsinggeluser.Model.Data;
 import com.example.jsongetsinggeluser.Model.Support;
+import com.example.jsongetsinggeluser.R;
 
 import java.util.List;
 
@@ -20,27 +24,31 @@ public class AdapterData extends RecyclerView.Adapter<AdapterData.HolderView> {
         this.context = context;
         this.dataList = dataList;
         this.supportList=supportList;
-
-
     }
 
     @NonNull
     @Override
     public HolderView onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_data,parent,false);
+        HolderView holderView = new HolderView(view);
+        return  holderView;
     }
 
     @Override
     public void onBindViewHolder(@NonNull HolderView holder, int position) {
+        Data dt =  dataList.get(position);
+        Support sp = supportList.get(position);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dataList.size()+supportList.size();
     }
 
     public class HolderView extends RecyclerView.ViewHolder{
+        TextView tv_id,tv_firs,tv_last,tv_email;
+        ImageView img_data;
 
         public HolderView(@NonNull View itemView) {
             super(itemView);
